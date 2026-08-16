89c0802e275d3448f354f3c313897fb6db5bf7b9
Coma <bmh4080@naver.com>
2026-08-16 23:01:04 +0900

    Add : join3, join4, stringify 유틸 매크로 추가.

--- cctl.h
+++ cctl.h
@@ -4,6 +4,10 @@
 #define cctl_concat_(A, B) A##B
 #define cctl_concat(A, B) cctl_concat_(A, B)
 #define cctl_join(PREFIX, NAME) cctl_concat(PREFIX, cctl_concat(_, NAME))
+#define cctl_join3(A, B, C) cctl_join(cctl_join(A, B), C)
+#define cctl_join4(A, B, C, D) cctl_join(cctl_join3(A, B, C), D)
+#define cctl_stringify_(TOKEN) #TOKEN
+#define cctl_stringify(TOKEN) cctl_stringify_(TOKEN)
 
 #define cctl_ptr(TYPE) cctl_join(TYPE, ptr)
 #define cctl_ptr_def(TYPE) \
