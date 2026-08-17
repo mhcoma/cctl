79626eb2150e3cfd6fa16963e673dcdbd13330b5
Coma <bmh4080@naver.com>
2026-08-17 16:57:51 +0900

    Add : list에 node 기반으로 동작하는 함수 추가.

--- list.h
+++ list.h
@@ -11,20 +11,20 @@
 #define node_struct(TYPE) cctl_join(node(TYPE), struct)
 
 #define node_imp_h(TYPE) \
-	typedef struct node_struct(TYPE) node(TYPE); \
-	\
-	struct node_struct(TYPE) { \
-		TYPE data; \
-		node(TYPE)* p_prev; \
-		node(TYPE)* p_next; \
-	}; \
-	\
-	void node_func(init, TYPE)(node(TYPE)* p_node);
+    typedef struct node_struct(TYPE) node(TYPE); \
+    \
+    struct node_struct(TYPE) { \
+        TYPE data; \
+        node(TYPE)* p_prev; \
+        node(TYPE)* p_next; \
+    }; \
+    \
+    void node_func(init, TYPE)(node(TYPE)* p_node);
 
 #define node_imp_c(TYPE) \
-	void node_func(init, TYPE)(node(TYPE)* p_node) { \
-		p_node->p_prev = p_node->p_next = NULL; \
-	}
+    void node_func(init, TYPE)(node(TYPE)* p_node) { \
+        p_node->p_prev = p_node->p_next = NULL; \
+    }
 
 #define list(TYPE) cctl_join(TYPE, list)
 #define list_func(FUNC, TYPE) cctl_join(list(TYPE), FUNC)
@@ -32,124 +32,172 @@
 
 #define list_init(TYPE, p_l) list_func(init, TYPE)(p_l)
 #define list_free(TYPE, p_l) list_func(free, TYPE)(p_l)
+#define list_clear(TYPE, p_l) list_func(clear, TYPE)(p_l)
 #define list_push_front(TYPE, p_l, item) list_func(push_front, TYPE)(p_l, item)
 #define list_push_back(TYPE, p_l, item) list_func(push_back, TYPE)(p_l, item)
 #define list_pop_front(TYPE, p_l) list_func(pop_front, TYPE)(p_l)
 #define list_pop_back(TYPE, p_l) list_func(pop_back, TYPE)(p_l)
-#define list_node_at(TYPE, p_l, index) list_func(at, TYPE)(p_l, index)
+#define list_insert(TYPE, p_l, p_node_node, item) list_func(insert, TYPE)(p_l, p_node_node, item)
+#define list_erase(TYPE, p_l, p_node_node) list_func(erase, TYPE)(p_l, p_node_node)
 
 #define list_node_front(TYPE, p_l) ((p_l)->p_front)
 #define list_node_back(TYPE, p_l) ((p_l)->p_back)
-#define list_front(TYPE, p_l) (&(list_node_front(TYPE, p_l)->data))
-#define list_back(TYPE, p_l) (&(list_node_back(TYPE, p_l)->data))
-#define list_at(TYPE, p_l, index) (&(list_func(node_at, TYPE)(p_l, index)->data))
+#define list_node_at(TYPE, p_l, index) list_func(node_at, TYPE)(p_l, index)
+
+#define list_front(TYPE, p_l) ((p_l)->p_front ? &((p_l)->p_front->data) : NULL)
+#define list_back(TYPE, p_l) ((p_l)->p_back ? &((p_l)->p_back->data) : NULL)
+#define list_at(TYPE, p_l, index) list_func(at, TYPE)(p_l, index)
 
 #define list_fd(TYPE) \
-	typedef struct list_struct(TYPE) list(TYPE);
+    typedef struct list_struct(TYPE) list(TYPE);
 
 #define list_imp_h(TYPE) \
-	node_imp_h(TYPE); \
-	\
-	struct list_struct(TYPE) { \
-		node(TYPE)* p_front; \
-		node(TYPE)* p_back; \
-		size_t size; \
-	}; \
-	\
-	void list_func(init, TYPE)(list(TYPE)* p_l); \
-	void list_func(free, TYPE)(list(TYPE)* p_l); \
-	bool list_func(push_front, TYPE)(list(TYPE)* p_l, TYPE item); \
-	bool list_func(push_back, TYPE)(list(TYPE)* p_l, TYPE item); \
-	void list_func(pop_back, TYPE)(list(TYPE)* p_l); \
-	void list_func(pop_front, TYPE)(list(TYPE)* p_l); \
-	node(TYPE)* list_func(node_at, TYPE)(list(TYPE)* p_l, size_t index);
+    node_imp_h(TYPE); \
+    \
+    struct list_struct(TYPE) { \
+        node(TYPE)* p_front; \
+        node(TYPE)* p_back; \
+        size_t size; \
+    }; \
+    \
+    void list_func(init, TYPE)(list(TYPE)* p_l); \
+    void list_func(free, TYPE)(list(TYPE)* p_l); \
+    void list_func(clear, TYPE)(list(TYPE)* p_l); \
+	node(TYPE)* list_func(push_front, TYPE)(list(TYPE)* p_l, TYPE item); \
+    node(TYPE)* list_func(push_back, TYPE)(list(TYPE)* p_l, TYPE item); \
+    bool list_func(pop_back, TYPE)(list(TYPE)* p_l); \
+    bool list_func(pop_front, TYPE)(list(TYPE)* p_l); \
+	node(TYPE)* list_func(insert_after_node, TYPE)(list(TYPE)* p_l, node(TYPE)* p_target, TYPE item); \
+    node(TYPE)* list_func(insert_before_node, TYPE)(list(TYPE)* p_l, node(TYPE)* p_target, TYPE item); \
+    bool list_func(remove_node, TYPE)(list(TYPE)* p_l, node(TYPE)* p_node); \
+    node(TYPE)* list_func(insert, TYPE)(list(TYPE)* p_l, size_t index, TYPE item); \
+    bool list_func(remove, TYPE)(list(TYPE)* p_l, size_t index); \
+    node(TYPE)* list_func(node_at, TYPE)(list(TYPE)* p_l, size_t index); \
+    TYPE* list_func(at, TYPE)(list(TYPE)* p_l, size_t index);
 
 #define list_imp_c(TYPE) \
-	node_imp_c(TYPE) \
-	\
-	void list_func(init, TYPE)(list(TYPE)* p_l) { \
-		p_l->p_front = p_l->p_back = NULL; \
-		p_l->size = 0; \
-	} \
-	\
-	void list_func(free, TYPE)(list(TYPE)* p_l) { \
-		while (p_l->size) { \
-			list_func(pop_back, TYPE)(p_l); \
-		} \
-	} \
-	\
-	bool list_func(push_front, TYPE)(list(TYPE)* p_l, TYPE item) { \
-		node(TYPE)* p_node = NULL; \
-		if (!(p_node = ((node(TYPE)*) malloc(sizeof(node(TYPE)))))) return false; \
-		node_func(init, TYPE)(p_node); \
-		p_node->data = item; \
-		if (p_l->p_front) { \
-			p_node->p_next = p_l->p_front; \
-			p_l->p_front->p_prev = p_node; \
-			p_l->p_front = p_node; \
-		} \
-		else p_l->p_back = p_l->p_front = p_node; \
-		p_l->size++; \
-		return true; \
-	} \
-	\
-	bool list_func(push_back, TYPE)(list(TYPE)* p_l, TYPE item) { \
-		node(TYPE)* p_node = NULL; \
-		if (!(p_node = ((node(TYPE)*) malloc(sizeof(node(TYPE)))))) return false; \
-		node_func(init, TYPE)(p_node); \
-		p_node->data = item; \
-		if (p_l->p_back) { \
-			p_node->p_prev = p_l->p_back; \
-			p_l->p_back->p_next = p_node; \
-			p_l->p_back = p_node; \
-		} \
-		else p_l->p_back = p_l->p_front = p_node; \
-		p_l->size++; \
-		return true; \
-	} \
-	\
-	void list_func(pop_front, TYPE)(list(TYPE)* p_l) { \
-		if (p_l->size) { \
-			node(TYPE)* p_node = p_l->p_front; \
-			p_l->p_front = p_node->p_next; \
-			if (p_l->p_front) \
-				p_l->p_front->p_prev = NULL; \
-			else \
-				p_l->p_back = NULL; \
-			p_l->size--; \
-			free(p_node); \
-		} \
-	} \
-	\
-	void list_func(pop_back, TYPE)(list(TYPE)* p_l) { \
-		if (p_l->size) { \
-			node(TYPE)* p_node = p_l->p_back; \
-			p_l->p_back = p_node->p_prev; \
-			if (p_l->p_back) \
-				p_l->p_back->p_next = NULL; \
-			else \
-				p_l->p_front = NULL; \
-			p_l->size--; \
-			free(p_node); \
-		} \
-	} \
-	\
-	node(TYPE)* list_func(node_at, TYPE)(list(TYPE)* p_l, size_t index) { \
-		bool direction = (index > (p_l->size / 2)); \
-		node(TYPE)* temp_node = direction ? list_node_back(TYPE, p_l) : list_node_front(TYPE, p_l); \
-		if (index == 0 || index == (p_l->size -1)) return temp_node; \
-		size_t temp_index = direction ? (p_l->size - 1) : 0; \
-		do { \
-			if (direction) { \
-				temp_index -= 1; \
-				temp_node = temp_node->p_prev; \
-			} \
-			else { \
-				temp_index += 1; \
-				temp_node = temp_node->p_next; \
-			} \
-		} while (temp_index != index); \
-		return temp_node; \
-	}
-	
+    node_imp_c(TYPE) \
+    \
+    void list_func(init, TYPE)(list(TYPE)* p_l) { \
+        p_l->p_front = p_l->p_back = NULL; \
+        p_l->size = 0; \
+    } \
+    \
+    void list_func(clear, TYPE)(list(TYPE)* p_l) { \
+        while (p_l->size > 0) { \
+            list_func(pop_back, TYPE)(p_l); \
+        } \
+    } \
+    \
+    void list_func(free, TYPE)(list(TYPE)* p_l) { \
+        list_func(clear, TYPE)(p_l); \
+    } \
+    \
+	node(TYPE)* list_func(push_front, TYPE)(list(TYPE)* p_l, TYPE item) { \
+        node(TYPE)* p_new_node = (node(TYPE)*) malloc(sizeof(node(TYPE))); \
+        if (!p_new_node) return NULL; \
+        p_new_node->data = item; \
+        p_new_node->p_prev = NULL; \
+        p_new_node->p_next = p_l->p_front; \
+        if (p_l->p_front) p_l->p_front->p_prev = p_new_node; \
+        else p_l->p_back = p_new_node; \
+        p_l->p_front = p_new_node; \
+        p_l->size++; \
+        return p_new_node; \
+    } \
+    \
+    node(TYPE)* list_func(push_back, TYPE)(list(TYPE)* p_l, TYPE item) { \
+        node(TYPE)* p_new_node = (node(TYPE)*) malloc(sizeof(node(TYPE))); \
+        if (!p_new_node) return NULL; \
+        p_new_node->data = item; \
+        p_new_node->p_next = NULL; \
+        p_new_node->p_prev = p_l->p_back; \
+        if (p_l->p_back) p_l->p_back->p_next = p_new_node; \
+        else p_l->p_front = p_new_node; \
+        p_l->p_back = p_new_node; \
+        p_l->size++; \
+        return p_new_node; \
+    } \
+    \
+    bool list_func(pop_front, TYPE)(list(TYPE)* p_l) { \
+        if (p_l->size == 0) return false; \
+        node(TYPE)* p_node_node = p_l->p_front; \
+        p_l->p_front = p_node_node->p_next; \
+        if (p_l->p_front) p_l->p_front->p_prev = NULL; \
+		else p_l->p_back = NULL; \
+        p_l->size--; \
+        free(p_node_node); \
+        return true; \
+    } \
+    \
+    bool list_func(pop_back, TYPE)(list(TYPE)* p_l) { \
+        if (p_l->size == 0) return false; \
+        node(TYPE)* p_node_node = p_l->p_back; \
+        p_l->p_back = p_node_node->p_prev; \
+        if (p_l->p_back) p_l->p_back->p_next = NULL; \
+		else p_l->p_front = NULL; \
+        p_l->size--; \
+        free(p_node_node); \
+        return true; \
+    } \
+    \
+	node(TYPE)* list_func(insert_after_node, TYPE)(list(TYPE)* p_l, node(TYPE)* p_target, TYPE item) { \
+        if (!p_target) return list_func(push_back, TYPE)(p_l, item); \
+        node(TYPE)* p_new_node = (node(TYPE)*) malloc(sizeof(node(TYPE))); \
+        if (!p_new_node) return NULL; \
+        p_new_node->data = item; \
+        p_new_node->p_prev = p_target; \
+        p_new_node->p_next = p_target->p_next; \
+        if (p_target->p_next) p_target->p_next->p_prev = p_new_node; \
+        else p_l->p_back = p_new_node; \
+        p_target->p_next = p_new_node; \
+        p_l->size++; \
+        return p_new_node; \
+    } \
+    \
+    node(TYPE)* list_func(insert_before_node, TYPE)(list(TYPE)* p_l, node(TYPE)* p_target, TYPE item) { \
+        if (!p_target) return list_func(push_front, TYPE)(p_l, item); \
+        node(TYPE)* p_new_node = (node(TYPE)*) malloc(sizeof(node(TYPE))); \
+        if (!p_new_node) return NULL; \
+        p_new_node->data = item; \
+        p_new_node->p_next = p_target; \
+        p_new_node->p_prev = p_target->p_prev; \
+        if (p_target->p_prev) p_target->p_prev->p_next = p_new_node; \
+        else p_l->p_front = p_new_node; \
+        p_target->p_prev = p_new_node; \
+        p_l->size++; \
+        return p_new_node; \
+    } \
+    \
+    bool list_func(remove_node, TYPE)(list(TYPE)* p_l, node(TYPE)* p_node) { \
+        if (!p_node || p_l->size == 0) return false; \
+        if (p_node == p_l->p_front) return list_func(pop_front, TYPE)(p_l); \
+        if (p_node == p_l->p_back) return list_func(pop_back, TYPE)(p_l); \
+        p_node->p_prev->p_next = p_node->p_next; \
+        p_node->p_next->p_prev = p_node->p_prev; \
+        free(p_node); \
+        p_l->size--; \
+        return true; \
+    } \
+	node(TYPE)* list_func(insert, TYPE)(list(TYPE)* p_l, size_t index, TYPE item) { \
+        if (index == 0) return list_func(push_front, TYPE)(p_l, item); \
+        if (index >= p_l->size) return list_func(push_back, TYPE)(p_l, item); \
+        node(TYPE)* p_target = list_func(node_at, TYPE)(p_l, index); \
+        return list_func(insert_before_node, TYPE)(p_l, p_target, item); \
+    } \
+    \
+    bool list_func(remove, TYPE)(list(TYPE)* p_l, size_t index) { \
+        if (index >= p_l->size) return false; \
+        if (index == 0) return list_func(pop_front, TYPE)(p_l); \
+        if (index == p_l->size - 1) return list_func(pop_back, TYPE)(p_l); \
+        node(TYPE)* p_node = list_func(node_at, TYPE)(p_l, index); \
+        return list_func(remove_node, TYPE)(p_l, p_node); \
+    } \
+    \
+    TYPE* list_func(at, TYPE)(list(TYPE)* p_l, size_t index) { \
+        node(TYPE)* p_node_node = list_func(node_at, TYPE)(p_l, index); \
+        if (!p_node_node) return NULL; \
+        return &(p_node_node->data); \
+    }
+
 #endif
